faf61da7bfa6596c055dd10b644ba1dca501a294
dori4n31 <58304780+dori4n31@users.noreply.github.com>
2021-04-07 00:09:09 -0500

    Delete build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-if [ -d "TruckDataset_TD" ] 
-then
-    echo "Directory TruckDataset_TD exists." 
-else
-    echo "Error: Directory TruckDataset_TD does not exists."
-    wget https://github.com/trangdao909/TruckDetective/raw/main/TruckDataset_TD.zip
-    unzip TruckDataset_TD.zip
-fi
